81cf7551d9c78ac26c3548236c1fb9fc90e44bcf
shafali03 <shaf03sm@gmail.com>
2020-03-07 20:17:32 +0000

    feat: click button change text

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hi Shaf'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         {this.state.string}
         </p>
+        <button onClick={() => this.setState({ string: 'Hi ALI' })}>
+        Change text
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +32,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
