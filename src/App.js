d2913b1a9e249b1f8d0ef10dd5a9cb6dceeb1d50
shafali03 <shaf03sm@gmail.com>
2020-03-08 13:38:18 +0000

    feat: displaying all user in one columns

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -19,9 +22,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <CardList name='Shaf'>
         {this.state.monsters.map(monster => (
-        <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+      </CardList>
     </div>
     )
   }
