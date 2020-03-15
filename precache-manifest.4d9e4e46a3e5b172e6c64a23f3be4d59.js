1ed94bfc5bda28a52a410ec5bb5b2bb35d474caf
shafali03 <shaf03sm@gmail.com>
2020-03-15 14:07:34 +0000

    Updates

---
+++ precache-manifest.4d9e4e46a3e5b172e6c64a23f3be4d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a7abb06663b4b2f415cd0b475e12e1",
+    "url": "/monsters-robot/index.html"
+  },
+  {
+    "revision": "4267f2604dfa70c7c4d1",
+    "url": "/monsters-robot/static/css/main.5b974f49.chunk.css"
+  },
+  {
+    "revision": "20f90a24066183785bca",
+    "url": "/monsters-robot/static/js/2.c82bd975.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-robot/static/js/2.c82bd975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4267f2604dfa70c7c4d1",
+    "url": "/monsters-robot/static/js/main.64ee184f.chunk.js"
+  },
+  {
+    "revision": "6021360eb8ebce5ea820",
+    "url": "/monsters-robot/static/js/runtime-main.63358b01.js"
+  }
+]);
